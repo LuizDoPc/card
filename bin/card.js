0ace1d3c6ee6c6b8c9134e79096a7bf5207811a5
luizdopc <luizotaviosoares@outlook.com>
2021-06-04 16:45:30 -0300

    update card

--- bin/card.js
+++ bin/card.js
@@ -7,32 +7,34 @@ const options = {
   padding: 1,
   margin: 2,
   borderStyle: 'double',
-  borderColor: '#F20089',
+  borderColor: '#163db7',
 };
 
 const data = {
   logo: chalk.white(`
-  
- _       _   _   _   ______       _____   _____       ___   _____     _____   _____  
-| |     | | | | | | |___  /      /  ___/ /  _  \\     /   | |  _  |   | ____| /  ___/ 
-| |     | | | | | |    / /       | |___  | | | |    / /| | | |_| |   | |__   | |___  
-| |     | | | | | |   / /        |___  \\ | | | |   / / | | |  __ |   |  __|  |___  \\ 
-| |___  | |_| | | |  / /__        ___| | | |_| |  / /  | | | |  \\ \\  | |___   ___| | 
-|_____| |_____/ |_| /_____|      /_____/ |_____/ /_/   |_| |_|   \\ \\ |_____| /_____/ 
-
-
+          ......             ............
+          ......                .........
+          ......                   ......
+          ......      .........      ....
+          ......    ..............      .
+          ......   ......    
+          ......    ...........
+          ......      ............
+          ......             .....
+          ......   ...............
+          ......     ...........
   `),
   name: chalk.white(' luizdopc /'),
-  mail: chalk.hex('#F20089')('luizsoares@ioasys.com.br'),
-  work: chalk.white('React Native @ ioasys'),
-  twitter: chalk.hex('#F20089')('https://twitter.com/luizdopc'),
-  github: chalk.hex('#F20089')('https://github.com/luizdopc'),
-  web: chalk.hex('#F20089')('https://luizdopc.github.io'),
+  mail: chalk.hex('#163db7')('luizotavioasoares@gmail.com'),
+  work: chalk.white('React Native @ ❤\n              ReactJS      @ olist'),
+  twitter: chalk.hex('#163db7')('https://twitter.com/luizdopc'),
+  github: chalk.hex('#163db7')('https://github.com/luizdopc'),
+  web: chalk.hex('#163db7')('https://bit.ly/3vWPQUD'),
   card: chalk.white('npx luizdopc'),
 };
 
 const label = {
-  work: chalk.white.bold('      Work:'),
+  work: chalk.white.bold('      Stack:'),
   twitter: chalk.white.bold('   Twitter:'),
   github: chalk.white.bold('    GitHub:'),
   web: chalk.white.bold('       Web:'),
@@ -40,17 +42,12 @@ const label = {
 };
 
 const output = `${data.logo}
-
 ${data.name} ${data.mail}
 
-
 ${label.work}  ${data.work}
-
 ${label.web}  ${data.web}
-
 ${label.twitter}  ${data.twitter}
 ${label.github}  ${data.github}
-
 ${label.card}  ${data.card}`;
 
 console.clear();
